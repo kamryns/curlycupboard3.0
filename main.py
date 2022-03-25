911695778073015198f61e0719119f4141e1096f
ranand2445 <riyaanand762@gmail.com>
2022-03-25 13:32:48 -0700

    tried fixing the error

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-from flask import render_template, request
+from flask import render_template
 from __init__ import app
 
-import requests
-import json
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
-@app.route('/dictionary/', methods=['GET','POST'])
-def dictionary():
-    try:
-        keyword = request.form['keyword']
-    except:
-        keyword = "study"
-    url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/"
-    querystring = {"entry":keyword}
-    headers = {
-        'x-rapidapi-host': "twinword-word-graph-dictionary.p.rapidapi.com",
-        'x-rapidapi-key': "3d43659d98msh26d5e705bc7d8b6p1d6431jsnba44357aaf20"
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    if response.status_code<400:
-        results = json.loads(response.content.decode("utf-8"))
-        return render_template("dictionary.html", results=results, word=keyword)
-    else:
-        return render_template("dictionary.html", word=keyword)
-    # print(response.text)
-
-
-@app.route('/notepad/')
-def notepad():
-    return render_template("notepad.html")
-
-# runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True, port=8081)
+    # runs the application on the repl development server
+    app.run(debug=True, port="8081")
