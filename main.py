07b4f44790174a7be692bf4f14c520e720a88995
ranand2445 <riyaanand762@gmail.com>
2022-04-07 22:24:23 -0700

    BASE CODE WORKING!! crud files implimented :)

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import render_template
 from __init__ import app
 
-@app.route('/index')
+from cruddy.app_crud import app_crud
+
+app.register_blueprint(app_crud)
+
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    # runs the application on the repl development server
-    app.run(debug=True, port="8081")
+    app.run(debug=True)
