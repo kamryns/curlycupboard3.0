a104ec69cf04379b30c52c547144cecec4d7eaf0
kamryns <kamrynsinsuan@gmail.com>
2022-04-17 20:01:07 -0700

    Revert "added phone number to sign up page"
    This reverts commit 48a3eea41a3cc619fc597ffb473cb80e0f7c3774.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import render_template
 from __init__ import app
-from flask_login import login_required
 
 from cruddy.app_crud import app_crud
 
@@ -10,11 +9,6 @@ app.register_blueprint(app_crud)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/tbr/")
-@login_required
-def tbr():
-    return render_template("tbr.html")
 
 
 if __name__ == "__main__":
