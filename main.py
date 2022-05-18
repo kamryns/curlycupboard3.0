0af8e229e7f3691a0f32b9fddc499afee2a107ce
briantang <brian.g.tang.135@gmail.com>
2022-05-18 07:00:30 -0700

    add dictionary in main.py

--- main.py
+++ main.py
@@ -36,7 +36,25 @@ def romeo():
 def echoice():
     return render_template("explorechoice.html")
 
-
+@app.route('/dictionary/', methods=['GET','POST'])
+def dictionary():
+    try:
+        keyword = request.form['keyword']
+    except:
+        keyword = "study"
+    url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/"
+    querystring = {"entry":keyword}
+    headers = {
+        'x-rapidapi-host': "twinword-word-graph-dictionary.p.rapidapi.com",
+        'x-rapidapi-key': "3d43659d98msh26d5e705bc7d8b6p1d6431jsnba44357aaf20"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if response.status_code<400:
+        results = json.loads(response.content.decode("utf-8"))
+        return render_template("dictionary.html", results=results, word=keyword)
+    else:
+        return render_template("dictionary.html", word=keyword)
+    # print(response.text)
 
 if __name__ == "__main__":
     # runs the application on the repl development server
